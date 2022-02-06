61e65e67c462e8a4d38afb5a0a1f17d786cb5c9c
AdamException <75698808+0xfce@users.noreply.github.com>
2022-02-06 22:55:44 +0100

    Some improvements
    now you can delete files without cd to them, and also you can delete a specific file extensions, or delete the whole folder content.

--- rmv.py
+++ rmv.py
@@ -1,42 +1,68 @@
-from sys import argv as arg
+from argparse import ArgumentParser as ArgParser
 from os import path, getcwd, listdir, remove, walk, chdir
 
-def rmv(dir: str):
-    with open(dir, mode='wb') as f:
-        f.write(bytes(0x0))
-        f.close()
-        remove(dir)
+parser = ArgParser()
+
+parser.add_argument('-s', '--specify', help='Deletes only a specific files extension', required=False)
+parser.add_argument('-p', '--path', help='Deletes only a specific file extension', required=True)
+
+args = parser.parse_args()
 
 def format(input: str) -> str:
-    return str(input).replace('\\', '/').replace('//', '/')
+    return input.replace('\\', '/').replace('//', '/')
 
-if __name__ == '__main__':
-    if path.isdir(arg[1]):
-        sub_dirs = [dir for dir in walk(arg[1])]
-        cwd = '%s/%s' % (format(getcwd()), arg[1],)
-        if len(listdir(cwd)) > 0:
-            for file in listdir(cwd):
-                try:
-                    print(f'{cwd}/{file} has been deleted')
-                    rmv(f'{cwd}/{file}')                    
-                except PermissionError:
-                    pass
+def rmv(dir: str):
+    dir = format(dir)
+    first_path = getcwd()
+    try:
+        if path.isdir(dir):
+            chdir(dir)
+            if not args.path == 'here' or not args.path == 'this' and not first_path == dir:
+                for files in listdir(dir):
+                    with open(files, mode='wb') as f:
+                        if args.specify:
+                            if f.name.endswith(f'.{args.specify}'):
+                                f.write(bytes(0x0))
+                                f.close()
+                                print(f'{f.name} has been deleted.')
+                                remove(f.name)                          
+                        else:
+                            f.write(bytes(0x0))
+                            f.close()
+                            print(f'{f.name} has been deleted.')
+                            remove(f.name)
+                chdir(args.path)
+        else:
+            with open(dir, mode='wb') as f:
+                f.write(bytes(0x0))
+                f.close()
+                print(f'{f.name} has been deleted.')
+                remove(f.name)            
+    except NotADirectoryError:
+        print('directory not found.')
+    except FileNotFoundError:
+        print('file not found.')
+    except PermissionError:
+        pass
+
+def execute(_path:str):
+    if path.isdir(_path):
+        sub_dirs = [dir for dir in walk(_path)]
+        if len(listdir(_path)) > 0:
+            for file in listdir(_path):
+                rmv(f'{_path}/{file}')                        
         if len(sub_dirs) > 1:
             for x in range(1, len(sub_dirs)):
-                cwd = cwd.replace(f'{arg[1]}', '')
-                chdir('%s/%s' % (format(cwd), format(sub_dirs[x][0]),))
+                chdir(format(sub_dirs[x][0]))
                 for j in range(0, len(sub_dirs[x])):
                     if not len(sub_dirs[x][j]) == 0:
                         for k in range(0, len(sub_dirs[x][j])):
-                            try:
-                                if not path.isdir(sub_dirs[x][j][k]):
-                                    for file in listdir('%s/%s' % (format(cwd), format(sub_dirs[x][0]),)):                                      
-                                        print('%s/%s/%s has been deleted.' % (format(cwd), format(sub_dirs[x][0]), file,))
-                                        rmv('%s/%s/%s' % (format(cwd), format(sub_dirs[x][0]), file,))
-                                        chdir(format(cwd))                            
-                            except PermissionError:
-                                pass
+                            if not path.isdir(sub_dirs[x][j][k]):
+                                for file in listdir(format(sub_dirs[x][0])):                                      
+                                    rmv('%s/%s' % (format(sub_dirs[x][0]), file,))
+                                    chdir(format(_path))                            
     else:
-        print(f'{arg[1]} has been deleted.')
-        rmv(arg[1])
+        rmv(_path)
 
+if __name__ == '__main__':
+    execute(getcwd() if args.path == 'this' or args.path == 'here' else args.path)
